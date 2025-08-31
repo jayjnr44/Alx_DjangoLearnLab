f9735f253e4da69c3072483fc463847e9d556693
Felix Kuffour <Jayycrypt@gmail.com>
2025-08-31 15:21:21 +0000

    intro to django

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Book
+
+# Custom admin for Book model
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+	list_display = ("title", "author", "publication_year")
+	list_filter = ("author", "publication_year")
+	search_fields = ("title", "author")
