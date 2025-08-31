05f5c43a6736442e9f2d05684c95182a0004b6cd
Felix Kuffour <Jayycrypt@gmail.com>
2025-08-31 15:31:43 +0000

    intro to django

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'book_shelf',
+    'bookshelf',
 ]
 
 MIDDLEWARE = [
--- Introduction_to_Django/LibraryProject/bookshelf/apps.py
+++ Introduction_to_Django/LibraryProject/bookshelf/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class BookShelfConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'book_shelf'
+    name = 'bookshelf'
