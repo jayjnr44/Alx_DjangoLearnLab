c24fc79000c0830accb883dd1d7eaaa5427e4e69
Felix Kuffour <Jayycrypt@gmail.com>
2025-09-15 23:00:24 +0000

    models in django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path("books/<int:book_id>/delete/", views.delete_book, name="delete_book"),
 
         # CRUD with permissions
-    path("books/add/", views.add_book, name="add_book"),
-    path("books/<int:book_id>/edit/", views.edit_book, name="edit_book"),
-    path("books/<int:book_id>/delete/", views.delete_book, name="delete_book"),
+    path("books/add/", views.add_book, name="add_book/"),
+    path("books/<int:book_id>/edit/", views.edit_book, name="edit_book/"),
+    path("books/<int:book_id>/delete/", views.delete_book, name="delete_book/"),
 ]
