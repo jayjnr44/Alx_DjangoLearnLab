5fadf33965ca8fa420001866d810986b4525f0ff
Felix Kuffour <Jayycrypt@gmail.com>
2025-09-15 21:26:55 +0000

    models in django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
-from . import views 
-from .views import list_books
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 
 urlpatterns = [
-    path('books/', views.list_books, name='list_books'),
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    # Login & Logout
+    path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
 
+    # Registration
     path("register/", views.register, name="register"),
-    path("login/", views.CustomLoginView.as_view(), name="login"),
-    path("logout/", views.CustomLogoutView.as_view(), name="logout"),
+
+    # Book list (after login redirect)
+    path("books/", views.list_books, name="list_books"),
 ]
