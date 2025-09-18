52995b6dcc16637f2b22d8247418428fbe7bd88c
Felix Kuffour <Jayycrypt@gmail.com>
2025-09-18 15:06:56 +0000

    security in django

---
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Book
+from django.core.exceptions import ValidationError  
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ["title", "author"]
+
+
+def validate_image(file):
+    max_mb = 5
+    if file.size > max_mb * 1024 * 1024:
+        raise ValidationError(f"Max file size is {max_mb}MB")
