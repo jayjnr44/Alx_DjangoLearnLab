e6b278861c815da00868422b89f2f80a896c5a62
Felix Kuffour <Jayycrypt@gmail.com>
2025-09-18 14:02:18 +0000

    security in django

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser,BaseUserManager
 
 
 # Create your models here.
@@ -16,3 +17,13 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to="profile_photos/", null=True, blank=True)
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
