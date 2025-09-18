600bd185710e0c29e78c47f9ad7ecc45525df25b
Felix Kuffour <Jayycrypt@gmail.com>
2025-09-18 14:04:56 +0000

    security in django

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 from .models import Book
 
@@ -9,3 +10,14 @@ class BookAdmin(admin.ModelAdmin):
 	search_fields = ('title', 'author')
 
 admin.site.register(Book, BookAdmin)
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Info", {"fields": ("date_of_birth", "profile_photo")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional Info", {"fields": ("date_of_birth", "profile_photo")}),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
