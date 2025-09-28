41b602577f0784db109192ffb4cbf6e78e46644a
Felix Kuffour <Jayycrypt@gmail.com>
2025-09-28 19:24:47 +0000

    added from django_filters import rest_framework

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from rest_framework.filters import SearchFilter,OrderingFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
+
 # Create your views here.
 
 
@@ -14,10 +16,10 @@ class BookListView(generics.ListCreateAPIView):
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    filterSet_fields = ['title', 'author', 'publication_year']
-    search_fields = ['title', 'author']
-    ordering_fields = ['title', 'publication_year'] #to allow ordering by these fields
-    ordering = ['title'] # default ordering
+    filterSet_fields = ["title", "author", "publication_year"]
+    search_fields = ["title", "author"]
+    ordering_fields = ["title", "publication_year"]  # to allow ordering by these fields
+    ordering = ["title"]  # default ordering
 
 
 # Retrieve a single book (read-only for everyone)
