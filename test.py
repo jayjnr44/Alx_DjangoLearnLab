1baaf913cbbebf7154d60275fb863295557127f2
Felix Kuffour <Jayycrypt@gmail.com>
2025-10-03 00:03:28 +0000

    deleted test files

--- test.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.test import APITestCase
-
-class DummyTest(APITestCase):
-    def test_dummy(self):
-        print("\n🚀 Running tests from the API app!")
-        self.assertTrue(True)
-    
