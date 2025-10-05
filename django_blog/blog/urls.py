33f42f1fb9d40c2e49cf5c754955b235e9f01183
Felix Kuffour <Jayycrypt@gmail.com>
2025-10-05 17:08:06 +0000

    configured urls for comment functionality

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -33,4 +33,20 @@ urlpatterns = [
         views.PostDeleteView.as_view(),
         name="post-delete-singular",
     ),
+    # Comment routes
+    path(
+        "posts/<int:post_pk>/comments/new/",
+        views.CommentCreateView.as_view(),
+        name="comment-create",
+    ),
+    path(
+        "comments/<int:pk>/update/",
+        views.CommentUpdateView.as_view(),
+        name="comment-update",
+    ),
+    path(
+        "comments/<int:pk>/delete/",
+        views.CommentDeleteView.as_view(),
+        name="comment-delete",
+    ),
 ]
