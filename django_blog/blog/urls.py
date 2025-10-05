ba96b07cd5c60f7c15f88db958440202ab7d0560
Felix Kuffour <Jayycrypt@gmail.com>
2025-10-05 16:27:46 +0000

    crud paths corrected

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,16 +3,8 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path(
-        "login/",
-        auth_views.LoginView.as_view(template_name="blog/login.html"),
-        name="login",
-    ),
-    path(
-        "logout/",
-        auth_views.LogoutView.as_view(template_name="blog/logout.html"),
-        name="logout",
-    ),
+    path("login/",auth_views.LoginView.as_view(template_name="blog/login.html"),name="login",),
+    path("logout/",auth_views.LogoutView.as_view(template_name="blog/logout.html"),name="logout"),
     #path("register/", views.register_view, name="register"),
     #path("profile/", views.profile_view, name="profile"),
     # Post CRUD
