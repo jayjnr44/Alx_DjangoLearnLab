46e03d4db0f8b062792cf26f0b98e355c3e38da1
Felix Kuffour <Jayycrypt@gmail.com>
2025-10-13 18:30:37 +0000

    added models for follow functionality

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -9,3 +9,21 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+    
+    def follow(self, user):
+        """Follow another user"""
+        if user != self:
+            self.following.add(user)
+    
+    def unfollow(self, user):
+        """Unfollow another user"""
+        if user != self:
+            self.following.remove(user)
+    
+    def is_following(self, user):
+        """Check if following another user"""
+        return self.following.filter(id=user.id).exists()
+    
+    def is_followed_by(self, user):
+        """Check if followed by another user"""
+        return self.followers.filter(id=user.id).exists()
