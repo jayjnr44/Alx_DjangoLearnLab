c8b0600d649b77343f86387171425e631dd24504
Felix Kuffour <Jayycrypt@gmail.com>
2025-10-13 15:36:25 +0000

    added token creation for user

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -10,16 +10,28 @@ class UserSerializer(serializers.ModelSerializer):
         fields= ['id', 'username', 'email', 'bio', 'profile_picture', 'followers', 'password']
         extra_kwargs = {'password': {'write_only' : True}}
     
+    #def create(self, validated_data):
     def create(self, validated_data):
+        # Extract fields that can't be passed to create_user
+        profile_picture = validated_data.pop('profile_picture', None)
+        
         user = get_user_model().objects.create_user(
-            username= validated_data['username'],
-            email = validated_data['email'],
-            password= validated_data['password'],
-            bio= validated_data.get('bio', '')
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            bio=validated_data.get('bio', '')
         )
+        
+        # Set additional fields after user creation
+        if profile_picture:
+            user.profile_picture = profile_picture
+            user.save()
+        
         # create an auth token for the newly created user
         Token.objects.create(user=user)
-        return user
+        return user        # create an auth token for the newly created user
+        #Token.objects.create(user=user)
+        #return user
     
 class LoginSerializer(serializers.Serializer):
         username = serializers.CharField()
