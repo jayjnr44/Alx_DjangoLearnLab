ee9f5e398959f5af564dd30f7a79055873e4913b
Felix Kuffour <Jayycrypt@gmail.com>
2025-10-13 15:22:02 +0000

    made views for register and login logics

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny,IsAuthenticated
+from rest_framework.views import APIView
+from .serializers import UserSerializer,LoginSerializer
+from .models import CustomUser
 
 # Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user= user)
+        return Response({'token': token.key, 'user_id': user.id, 'username': user.username})
