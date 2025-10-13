fdf43b2e6edec0453ac55e3d6fe9617bbedfc52b
Felix Kuffour <Jayycrypt@gmail.com>
2025-10-13 15:19:50 +0000

    minor changes

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    bio = models.TextField(blank =True, name=True)
-    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    bio = models.TextField(blank=True),    
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True),
     followers =models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
 
     def __str__(self):
